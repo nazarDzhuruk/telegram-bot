ec03fb34770b15dcfca732a93d97a574581374fb
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2022-03-11 13:16:55 +0100

    Delete useless code

--- src/main/java/com/foxminded/telebot/TelegramTestBotApplication.java
+++ src/main/java/com/foxminded/telebot/TelegramTestBotApplication.java
@@ -1,28 +1,12 @@
 package com.foxminded.telebot;
 
-import com.foxminded.telebot.model.Genre;
-import com.foxminded.telebot.service.GenreService;
-import com.foxminded.telebot.service.HtmlDataParser;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 @SpringBootApplication
 public class TelegramTestBotApplication {
-    public static GenreService genreService;
 
-    public TelegramTestBotApplication(GenreService genreService) {
-        this.genreService = genreService;
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(TelegramTestBotApplication.class);
 
     }
