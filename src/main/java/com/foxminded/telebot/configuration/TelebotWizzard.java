8638f9fd580105259aaffd40e290988c6142bc04
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2022-03-11 13:18:02 +0100

    Delete useless code, add updates handler

--- src/main/java/com/foxminded/telebot/configuration/TelebotWizzard.java
+++ src/main/java/com/foxminded/telebot/configuration/TelebotWizzard.java
@@ -1,34 +1,32 @@
 package com.foxminded.telebot.configuration;
 
 import com.foxminded.telebot.exception.TelebotWizzardException;
-import com.foxminded.telebot.keyboard.KeyboardService;
-import com.foxminded.telebot.repository.GenreRepository;
+import com.foxminded.telebot.handler.Command;
+import com.foxminded.telebot.handler.HandlerFactory;
 import com.foxminded.telebot.service.GenreService;
 import com.foxminded.telebot.service.HtmlDataParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import javax.annotation.PostConstruct;
+import java.util.Locale;
 
 @Component
 public class TelebotWizzard extends TelegramLongPollingBot {
     private final BotConfiguration configuration;
     private final GenreService genreService;
-    private final KeyboardService keyboardService;
-
+    private final HandlerFactory handleMessage;
 
     @Autowired
-    public TelebotWizzard(BotConfiguration configuration, GenreService genreService, KeyboardService keyboardService) {
+    public TelebotWizzard(HandlerFactory handleMessage, BotConfiguration configuration, GenreService genreService) {
         this.configuration = configuration;
         this.genreService = genreService;
-        this.keyboardService = keyboardService;
+        this.handleMessage = handleMessage;
     }
 
     @PostConstruct
@@ -57,14 +55,10 @@ public class TelebotWizzard extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.getMessage() != null && update.getMessage().hasText()) {
-            long chatId = update.getMessage().getChatId();
-            String userName = update.getMessage().getChat().getUserName();
-
             try {
-                execute(SendMessage.builder().chatId(String.valueOf(chatId))
-                        .text("hi " + userName)
-                        .replyMarkup(InlineKeyboardMarkup.builder()
-                                .keyboard(keyboardService.genreButtons(genreService.getAll())).build()).build());
+                execute(handleMessage.handleUpdate(Command.valueOf(update.getMessage()
+                                .getText().toUpperCase(Locale.ROOT)))
+                        .handleMessage(update.getMessage()));
             } catch (TelegramApiException e) {
                 throw new TelebotWizzardException(e.getMessage());
             }
