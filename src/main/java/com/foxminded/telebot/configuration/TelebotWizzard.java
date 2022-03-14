7d5569adb7dd0827bf6680035a548178ac787011
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2022-03-14 12:18:19 +0100

    Hardcodded

--- src/main/java/com/foxminded/telebot/configuration/TelebotWizzard.java
+++ src/main/java/com/foxminded/telebot/configuration/TelebotWizzard.java
@@ -1,7 +1,8 @@
 package com.foxminded.telebot.configuration;
 
 import com.foxminded.telebot.exception.TelebotWizzardException;
-import com.foxminded.telebot.handler.Command;
+import com.foxminded.telebot.handler.callback.Callback;
+import com.foxminded.telebot.handler.message.MessageCommand;
 import com.foxminded.telebot.handler.HandlerFactory;
 import com.foxminded.telebot.service.GenreService;
 import com.foxminded.telebot.service.HtmlDataParser;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -21,6 +23,7 @@ public class TelebotWizzard extends TelegramLongPollingBot {
     private final BotConfiguration configuration;
     private final GenreService genreService;
     private final HandlerFactory handleMessage;
+
 
     @Autowired
     public TelebotWizzard(HandlerFactory handleMessage, BotConfiguration configuration, GenreService genreService) {
@@ -35,7 +38,7 @@ public class TelebotWizzard extends TelegramLongPollingBot {
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(this);
-        } catch (Exception e) {
+        } catch (TelegramApiException e) {
             throw new TelebotWizzardException(e.getMessage());
         }
 
@@ -56,12 +59,38 @@ public class TelebotWizzard extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         if (update.getMessage() != null && update.getMessage().hasText()) {
             try {
-                execute(handleMessage.handleUpdate(Command.valueOf(update.getMessage()
-                                .getText().toUpperCase(Locale.ROOT)))
-                        .handleMessage(update.getMessage()));
+                Message message = update.getMessage();
+                String command = message.getText().replace("/", "");
+                execute(handleMessage.handleUpdate(MessageCommand.valueOf(command.toUpperCase(Locale.ROOT)))
+                        .handleMessage(message));
             } catch (TelegramApiException e) {
                 throw new TelebotWizzardException(e.getMessage());
+            }
+        } else if (update.hasCallbackQuery()) {
+            String callbackData = update.getCallbackQuery().getData();
+            boolean one = genreService.getAll().stream().anyMatch(g -> g.getGenreName().equals(callbackData));
+
+            if (one) {
+                handleMessage.handleCallBack(Callback.GENRES)
+                        .getDescription(update.getCallbackQuery()).forEach(s -> {
+                            try {
+                                execute(s);
+                            } catch (TelegramApiException e) {
+                                e.printStackTrace();
+                            }
+                        });
+            }
+            else {
+                handleMessage.handleCallBack(Callback.SHOW)
+                        .getDescription(update.getCallbackQuery()).forEach(s -> {
+                            try {
+                                execute(s);
+                            } catch (TelegramApiException e) {
+                                e.printStackTrace();
+                            }
+                        });
             }
         }
     }
 }
+
