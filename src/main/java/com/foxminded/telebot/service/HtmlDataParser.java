141f4ec8e40e68253d400d9b3488504f716331ec
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2022-03-17 17:49:33 +0100

    Replace links to StaticLink, add new features

--- src/main/java/com/foxminded/telebot/service/HtmlDataParser.java
+++ src/main/java/com/foxminded/telebot/service/HtmlDataParser.java
@@ -2,6 +2,7 @@ package com.foxminded.telebot.service;
 
 import com.foxminded.telebot.exception.HtmlDataParserException;
 import com.foxminded.telebot.model.Genre;
+import com.foxminded.telebot.model.StaticLink;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -13,10 +14,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class HtmlDataParser {
-    private static final String LINK = "https://kinogo.film";
-    private static final String ELEMENTS_CLASS = "miniblock";
-    private static final String HTML_ELEMENT = "a";
-    private static final String HTML_ATTRIBUTE = "href";
+
     private static Document document;
     private static Document mapper;
 
@@ -25,7 +23,7 @@ public class HtmlDataParser {
 
     static {
         try {
-            document = Jsoup.connect(LINK).get();
+            document = Jsoup.connect(StaticLink.MAIN_LINK).get();
         } catch (IOException e) {
             throw new HtmlDataParserException(e.getMessage());
         }
@@ -39,27 +37,37 @@ public class HtmlDataParser {
     }
 
     public static List<String> getTitles(String genreLink, String pageNumber) {
-        String path = LINK + genreLink + pageNumber;
+        String path = StaticLink.MAIN_LINK + genreLink + pageNumber;
         try {
             mapper = Jsoup.connect(path).get();
         } catch (IOException e) {
             throw new HtmlDataParserException(e.getMessage());
         }
-        return mapper.getElementsByClass("zagolovki").eachText();
+        return mapper.getElementsByClass(StaticLink.ELEMENTS_CLASS_ZAGOLOVKI).eachText();
     }
 
 
     public static List<String> getFilmsLink(String genreLink, String pageNumber) {
-        String path = LINK + genreLink + pageNumber;
+        String path = StaticLink.MAIN_LINK + genreLink + pageNumber;
         try {
             mapper = Jsoup.connect(path).get();
         } catch (IOException e) {
             throw new HtmlDataParserException(e.getMessage());
         }
-        return mapper.getElementsByClass("zagolovki").select("a")
-                .eachAttr("href").stream()
-                .map(s -> s.split(LINK)[1])
-                .map(s -> s.replace(".html", "")).collect(Collectors.toList());
+        return mapper.getElementsByClass(StaticLink.ELEMENTS_CLASS_ZAGOLOVKI).select(StaticLink.HTML_ELEMENT_A)
+                .eachAttr(StaticLink.HREF).stream()
+                .map(s -> s.split(StaticLink.MAIN_LINK)[1])
+                .map(s -> s.replace(StaticLink.DOT_HTML, StaticLink.EMPTY_STRING)).collect(Collectors.toList());
+    }
+
+    public static List<String> getFilmRating(String filmLink) {
+        String path = StaticLink.MAIN_LINK + filmLink;
+        try {
+            mapper = Jsoup.connect(path).get();
+        } catch (IOException e) {
+            throw new HtmlDataParserException(e.getMessage());
+        }
+        return mapper.getElementsByClass(StaticLink.RATING_DIGITS).eachText();
     }
 
     public static String getFilmDesc(String path) {
@@ -68,20 +76,21 @@ public class HtmlDataParser {
         } catch (IOException e) {
             throw new HtmlDataParserException(e.getMessage());
         }
-        return mapper.getElementsByClass("fullimg").text();
+        return mapper.getElementsByClass(StaticLink.FULLIMG).text().replace(".", ".\n")
+                .split("Год")[0];
     }
 
     private static List<String> getGenresAsText() {
 
-        Elements element = document.getElementsByClass(ELEMENTS_CLASS);
+        Elements element = document.getElementsByClass(StaticLink.ELEMENTS_CLASS_MINIBLOCK);
 
-        return new ArrayList<>(element.select(HTML_ELEMENT).eachText());
+        return new ArrayList<>(element.select(StaticLink.HTML_ELEMENT_A).eachText());
     }
 
     private static List<String> getGenresLinksFromPage() {
 
-        Elements element = document.getElementsByClass(ELEMENTS_CLASS);
+        Elements element = document.getElementsByClass(StaticLink.ELEMENTS_CLASS_MINIBLOCK);
 
-        return new ArrayList<>(element.select(HTML_ELEMENT).eachAttr(HTML_ATTRIBUTE));
+        return new ArrayList<>(element.select(StaticLink.HTML_ELEMENT_A).eachAttr(StaticLink.HREF));
     }
 }
