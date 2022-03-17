db50698816dfb8846bf32ebe126880a02c93a3fc
Nazar Dzhuruk <nazar.dzhuruk@gmail.com>
2022-03-17 17:47:27 +0100

    Replace links to static class

---
+++ src/main/java/com/foxminded/telebot/model/StaticLink.java
@@ -0,0 +1,13 @@
+package com.foxminded.telebot.model;
+
+public class StaticLink {
+    public static final String MAIN_LINK = "https://kinogo.film";
+    public static final String ELEMENTS_CLASS_MINIBLOCK = "miniblock";
+    public static final String ELEMENTS_CLASS_ZAGOLOVKI = "zagolovki";
+    public static final String HTML_ELEMENT_A = "a";
+    public static final String HREF = "href";
+    public static final String DOT_HTML = ".html";
+    public static final String EMPTY_STRING = "";
+    public static final String RATING_DIGITS = "rating_digits";
+    public static final String FULLIMG = "fullimg";
+}
